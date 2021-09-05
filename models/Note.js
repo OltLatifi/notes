1bb047702131d2a56475d4afaaa6495c4829c50f
Olt Latifi <oltlatifi2003@gmail.com>
2021-09-05 13:35:56 +0200

    connection with database to make API's

---
+++ models/Note.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+
+const noteSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        trim: true,
+        unique: false,
+        required: [true, 'Please add a title'],
+        maxLength: [40, 'Title cannot be more than 40 charcaters']
+    },
+    description: {
+        type: String,
+        required: false,
+        maxLength: [200, 'Description cannot be more than 200 characters']
+    }
+})
+
+module.exports = mongoose.models.Note || mongoose.model('Note', noteSchema)
--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
   reactStrictMode: true,
+  env:{
+    MONGO_URI: "mongodb+srv://OltLatifi:RxJPhJ4MUbGuSASo@cluster0.twdgf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  },
 }
