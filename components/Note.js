cd3407c211a64fb030e9d7c87bafcd2621670189
Olt Latifi <oltlatifi2003@gmail.com>
2021-09-07 13:26:22 +0200

    buttons stick to the bottom os the card

--- components/Note.js
+++ components/Note.js
@@ -1,22 +1,32 @@
-import {Card, CardContent, Typography} from '@material-ui/core'
+import {Card, CardContent, CardActions, Typography, Button} from '@material-ui/core'
 import styles from '../styles/Note.module.css'
 import Link from 'next/link'
 
 function Note(props) {
     return (
         <>
-        <Link href={`/notes/${props.id}`}>
-        <Card className={styles.note}>
-            <CardContent>
-                <Typography gutterBottom variant="h5" component="h2">
-                    {props.title}
-                </Typography>
-                <Typography variant="body2" color="textSecondary" component="p">
-                    {props.description}
-                </Typography>
-            </CardContent>
+        
+        <Card className={styles.card}>
+            <Link href={`/notes/${props.id}`}>
+                <CardContent className={styles.note}>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {props.title}
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {props.description}
+                    </Typography>
+                </CardContent>
+            </Link>
+            <CardActions >
+                <Button size="small" variant="contained">
+                    Edit
+                </Button>
+                <Button size="small" variant="contained" style={{backgroundColor:'pink'}}>
+                    Delete
+                </Button>
+            </CardActions>
         </Card>
-        </Link>
+        
         </>
     )
 }
--- pages/notes/[id]/index.js
+++ pages/notes/[id]/index.js
@@ -15,7 +15,7 @@ function Note({note}) {
         </div>
     )
 }
-
+// query: { id } -- gets the id from the router
 Note.getInitialProps = async ({ query: { id } }) => {
     const res = await fetch(`http://localhost:3000/api/notes/${id}`);
     const { data } = await res.json();
