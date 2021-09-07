d1909b9b2d0d19862c0594ca9be833a3bf4a9ac3
Olt Latifi <oltlatifi2003@gmail.com>
2021-09-07 20:21:26 +0200

    getting ready for deployment

--- pages/note.js
+++ pages/note.js
@@ -1,7 +1,60 @@
-import Form from '../components/Form'
+import {TextField, Typography, Button, Card} from '@material-ui/core'
+import styles from '../styles/Form.module.css'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import axios from 'axios'
 
-function note() {
-    return <Form/>
+
+function Form() {
+    const router = useRouter()
+    const[title, setTitle] = useState('')
+    const[description, setDescription] = useState('')
+
+    const buttonPressed = ()=>{
+        // title is required
+        if(title!==''){
+            axios.post('/api/notes/', {
+                "title": title,
+                "description": description,
+            })
+            .then(response => router.push('/'))
+        }else{
+            alert("You can't make a note without a title")
+        }
+        
+    }
+
+    return (
+        <center>
+            <Card  className={styles.form}>
+            <Typography variant="h5">Enter a note</Typography><br/>
+            <form noValidate autoComplete="off" >
+                <TextField
+                    id="standard-basic"
+                    label="Title"
+                    className={styles.input}
+                    inputProps={{ maxLength: 40 }}
+                    onChange={(e)=>{setTitle(e.target.value)}}/>
+                <br/><br/>
+                <TextField
+                    id="standard-multiline-static"
+                    label="Description"
+                    className={styles.input}
+                    inputProps={{ maxLength: 200 }}
+                    onChange={(e)=>{setDescription(e.target.value)}}
+                    multiline
+                    rows={6}
+                    />
+                <br/><br/>
+                <Button
+                    variant="contained"
+                    className={styles.input}
+                    onClick={buttonPressed}>Submit</Button>
+            </form>
+            </Card>
+        </center>
+        
+    )
 }
 
-export default note
+export default Form
